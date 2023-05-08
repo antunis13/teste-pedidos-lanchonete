4c27d9c9fb04ec99abb91321ed426889e72c1de2
Richard Antunis <richardantunis4@gmail.com>
2023-05-08 15:32:22 -0300

    adding remove clients function

--- views/js/lists.js
+++ views/js/lists.js
@@ -3,23 +3,49 @@ const productsBtn = document.querySelector('#products')
 const ordersBtn = document.querySelector('#orders')
 
 
+function eventoRemoverClientes(){
+    const removeBtn = document.querySelectorAll('#remove-btn')
+
+    removeBtn.forEach(button => {
+        button.onclick = function(e){
+            e.preventDefault()
+            const id = this.dataset.id
+            console.log(id)
+
+            fetch(`http://localhost:8080/api/clients/${id}`, {
+            method: 'DELETE',
+            }).then(response => {
+                response.json().then(data =>{
+                    if(data.message = 'success'){
+                        alert('Cliente excluído com sucesso')
+                        obterListaClientes()
+                    }else{
+                        alert('Ocorreu um erro')
+                    }
+                })
+            })
+        }
+    })
+}
 function obterListaClientes(){
     const clientsList = document.querySelector('#clients-list')
 
     fetch('http://localhost:8080/api/clients/').then(response =>{
         response.json().then(data =>{
             const clientsHtml = data.map(clients => `
-                <li>
-                   Nome: ${clients.name} <br>
-                   Email: ${clients.email} <br>
-                   Telefone: ${clients.tel} <br>
-                   Endereço: ${clients.address} <br>
-
-                    <a href="#" class="remove-btn" data-id"${clients.id}">Excluir</a>
-                    <hr>
-                </li>
+            <li>
+            Nome: ${clients.name} <br>
+            Email: ${clients.email} <br>
+            Telefone: ${clients.tel} <br>
+            Endereço: ${clients.address} <br>
+            
+            <a href="#" id="remove-btn" data-id="${clients._id}">Excluir</a>
+            <hr>
+            </li>
             `).join('')
             clientsList.innerHTML = clientsHtml
+            
+            eventoRemoverClientes()
         })
     })
 }
@@ -34,7 +60,7 @@ function obterListaProdutos(){
                    Nome: ${products.name} <br>
                    Price: ${products.price} <br>
                 
-                    <a href="#" class="remove-btn" data-id"${products.id}">Excluir</a>
+                    <a href="#" id="remove-btn" data-id"${products.id}">Excluir</a>
                     <hr>
                 </li>
             `).join('')
